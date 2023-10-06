86fee9f2f38e8d9cabe89b78a647bd8a620db4a6
Timothy Meleki <muyiwameleki@gmail.com>
2023-10-06 19:46:41 +0100

    initial commit

---
+++ InvoiceTest/Invoice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceTest
+{
+    internal class Invoice
+    {
+        private string partNumber;
+        private string partDescription;
+        private int quantity;
+        private decimal pricePerItem;
+
+        public string PartNumber { get; set; }
+        public string PartDescription { get; set; }
+        private int Quantity 
+        {
+           set
+            {
+                if (value > 0)
+                {
+                    quantity = value;
+                }
+            }
+            get
+            {
+                return quantity;
+            }
+        }
+        private decimal PricePerItem
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    pricePerItem = value;
+                }
+            }
+            get
+            {
+                return pricePerItem;
+            }
+        }
+        public Invoice(int quantity, decimal price)
+        {
+            PartNumber = string.Empty;
+            PartDescription = string.Empty;
+            Quantity = quantity;
+            PricePerItem = price;
+        }
+
+        public decimal GetInvoiceAmount ()
+        {
+            decimal price = PricePerItem * Quantity;
+            return price;
+        }
+
+        public string DisplayInvoice ()
+        {
+            return $"\nThe product: {PartNumber}\nProduct Description: {PartDescription}\nQuantity Purchased: {Quantity}\nThe Price: {GetInvoiceAmount()}";
+        }
+    }
+}
---
+++ InvoiceTest/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace InvoiceTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Part Number of product: ");
+            string partNumber = Console.ReadLine();
+
+            Console.Write("Enter the Part Description of product: ");
+            string partDescription = Console.ReadLine();
+
+            Console.Write("Enter quantity purchased: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter price per item: ");
+            decimal pricePerItem = Convert.ToDecimal(Console.ReadLine());
+
+            Invoice invoice = new Invoice(quantity, pricePerItem);
+
+            invoice.PartNumber = partNumber;
+            invoice.PartDescription = partDescription;
+
+
+            Console.WriteLine(invoice.DisplayInvoice());
+        }
+    }
+}
